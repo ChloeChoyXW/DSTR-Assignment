8f26eab3a5a079fa70b4a3c58a33d94fc974d1f2
Chloe Choy <chloechoy66@gmail.com>
2023-05-20 01:58:03 +0800

    Made changes on the excel file as well as read file in University.cpp

--- DSTR Assignment/University.cpp
+++ DSTR Assignment/University.cpp
@@ -1,4 +1,9 @@
 #include "university.h"
+#include <fstream>
+#include <sstream>
+
+
+using namespace  std;
 
 
 UniList::UniList(string uniListname) : uniListName(uniListName) {};
@@ -7,6 +12,7 @@ Uni* UniList::createNewNode(int rank, string instName, string locationCode, stri
 	float employerRepScore, int employerRepRank, float facultyStuRIScore, int facultyStuRIRank, float citePerFacultyScore,
 	int citePerFacultyRank, float intFacultyRIScore, int intFacultyRIRank, float intStuRIScore, int intStuRIRank,
 	float intResearchNetScore, int intResearchNetRank, float employOutcomeScore, int employOutcomeRank, float scoreScale) {
+
 
 	Uni* newnode = new Uni;
 
@@ -32,6 +38,7 @@ Uni* UniList::createNewNode(int rank, string instName, string locationCode, stri
 	newnode->employOutcomeRank = employOutcomeRank;
 	newnode->scoreScale = scoreScale;
 	newnode->nextAdd = NULL;
+	newnode->prevAdd = NULL;
 
 	return newnode;
 
@@ -60,63 +67,72 @@ void UniList::insertToEndOfUniLinkedList(int rank, string instName, string locat
 
 void UniList::readFromFile() {
 	head = NULL; //this list is empty now
+	int count = 0;
 
 	string rank, instName, locationCode, location, academicScore, academicRank, employerRepScore, employerRepRank,
 		facultyStuRIScore, facultyStuRIRank, citePerFacultyScore, citePerFacultyRank, intFacultyRIScore, intFacultyRIRank,
 		intStuRIScore, intStuRIRank, intResearchNetScore, intResearchNetRank, employOutcomeScore, employOutcomeRank, scoreScale;
 
-	/*ifstream file("2023_QS_World_University_Rankings.csv");
+	ifstream file("2023_QS_World_University_Rankings.csv");
+	string line;
 
+	//getline(file, line)
 	while (file.good())
 	{
+		stringstream ss(line);
 		getline(file, rank, ',');
 		getline(file, instName, ',');
 		getline(file, locationCode, ',');
-		getline(file, location);
+		getline(file, location, ',');
 		getline(file, academicScore, ',');
 		getline(file, academicRank, ',');
-		getline(file, employerRepScore);
+		getline(file, employerRepScore, ',');
 		getline(file, employerRepRank, ',');
 		getline(file, facultyStuRIScore, ',');
-		getline(file, facultyStuRIRank);
+		getline(file, facultyStuRIRank, ',');
 		getline(file, citePerFacultyScore, ',');
 		getline(file, citePerFacultyRank, ',');
-		getline(file, intFacultyRIScore);
+		getline(file, intFacultyRIScore, ',');
 		getline(file, intFacultyRIRank, ',');
 		getline(file, intStuRIScore, ',');
-		getline(file, intStuRIRank);
+		getline(file, intStuRIRank, ',');
 		getline(file, intResearchNetScore, ',');
-		getline(file, intResearchNetRank);
+		getline(file, intResearchNetRank, ',');
 		getline(file, employOutcomeScore, ',');
-		getline(file, employOutcomeRank);
-		getline(file, scoreScale, ',');*/
+		getline(file, employOutcomeRank, ',');
+		getline(file, scoreScale);
 
-		/*if (rank == "Rank")
+		if (count == 0)
 		{
+			count += 1;
 			continue;
 		}
 		else if (rank == "")
 		{
 			break;
-		}*/
+		}
 
-		//try {
-			/*insertToEndOfUniLinkedList(stoi(rank), instName, locationCode, location, stof(academicScore), stoi(academicRank), stof(employerRepScore),
+		try {
+			insertToEndOfUniLinkedList(stoi(rank), instName, locationCode, location, stof(academicScore), stoi(academicRank), stof(employerRepScore),
 				stoi(employerRepRank), stof(facultyStuRIScore), stoi(facultyStuRIRank), stof(citePerFacultyScore), stoi(citePerFacultyRank),
 				stof(intFacultyRIScore), stoi(intFacultyRIRank), stof(intStuRIScore), stoi(intStuRIRank), stof(intResearchNetScore),
-				stoi(intResearchNetRank), stof(employOutcomeScore), stoi(employOutcomeRank), stof(scoreScale));*/
-	insertToEndOfUniLinkedList(3, "instNameD", "RlocationCode", "locXation", 10, 21, 101, 78, 81, 91, 81, 11, 781, 531, 561, 31, 321, 167, 421, 145, 21);
-	insertToEndOfUniLinkedList(2, "instNameA", "YlocationCode", "locLation", 9, 122, 22, 23, 42, 21, 28, 22, 42, 42, 32, 12, 892, 242, 426, 223, 24);
-	insertToEndOfUniLinkedList(1, "instNameH", "HlocationCode", "locJation", 31, 13, 31, 73, 36, 63, 83, 34, 543, 243, 33, 83, 93, 43, 53, 637, 234);
-	insertToEndOfUniLinkedList(4, "instNameO", "JlocationCode", "locWation", 1, 143, 10, 91, 11, 71, 71, 15, 91, 132, 31, 91, 91, 18, 16, 414, 112);
-	insertToEndOfUniLinkedList(5, "instNameE", "KlocationCode", "locEation", 16, 10, 8, 13, 21, 94, 11, 17, 81, 16, 671, 11, 41, 18, 16, 31, 31);
+				stoi(intResearchNetRank), stof(employOutcomeScore), stoi(employOutcomeRank), stof(scoreScale));
 
-	/*}
-	catch(exception e){
-		cout << "Error occured. Please try again later." << endl;
-	}*/
+			/*insertToEndOfUniLinkedList(rank, instName, locationCode, location, academicScore, academicRank, employerRepScore,
+				employerRepRank, facultyStuRIScore, facultyStuRIRank, citePerFacultyScore, citePerFacultyRank,
+				intFacultyRIScore, intFacultyRIRank, intStuRIScore, intStuRIRank, intResearchNetScore,
+				intResearchNetRank, employOutcomeScore, employOutcomeRank, scoreScale);*/
+			
 
-	//}
+		}
+		catch (exception e) {
+			cout << "Exception:" << e.what()<<endl;
+			cout << instName << endl << rank <<endl<< endl;
+			//cout << "Error occured. Please try again later." << endl;
+		}
+
+	}
+	
 }
 
 void UniList::searchUni(string searchCondition) {
@@ -585,6 +601,7 @@ void UniList::insertionSortUniDoublyLinkedList(int sortCondition) {
 	//	10 = intResearchNetRank
 	//	11 = employOutcomeRank
 	//	12 = scoreScale*/
+	// 
 		//insertionSort(&head, sortCondition);
 		// Initialize 'sorted' - a sorted doubly linked list
 	Uni* sorted = NULL;
@@ -618,35 +635,50 @@ void UniList::insertionSortUniDoublyLinkedList(int sortCondition) {
 
 void UniList::displayList() {
 	Uni* current = head;
+	bool  contPrint = true;
+	string cont = "y";
 	//check if is last node, else print
-	while (current != NULL) {
-		cout << "University Name: " << current->instName << endl;
-		cout << "Rank: " << current->rank << endl;
-		cout << "Location Code: " << current->locationCode << endl;
-		cout << "Location: " << current->location << endl;
-		cout << "Academic Score " << current->academicScore << endl;
-		cout << "Academic Rank: " << current->academicRank << endl;
-		cout << "Employer Reputation Score: " << current->employerRepScore << endl;
-		cout << "Employer Reputation Rank: " << current->employerRepRank << endl;
-		cout << "Faculty/Student Ratio Score: " << current->facultyStuRIScore << endl;
-		cout << "Faculty/Student Ratio Rank: " << current->facultyStuRIRank << endl;
-		cout << "Citations Per Faculty Score: " << current->citePerFacultyScore << endl;
-		cout << "Citations Per Faculty Rank: " << current->citePerFacultyRank << endl;
-		cout << "International Faculty Ratio Score: " << current->intFacultyRIScore << endl;
-		cout << "International Faculty Ratio Rank: " << current->intFacultyRIRank << endl;
-		cout << "International Student Ratio Score: " << current->intStuRIScore << endl;
-		cout << "International Student Ratio Rank: " << current->intStuRIRank << endl;
-		cout << "International Research Network Score: " << current->intResearchNetScore << endl;
-		cout << "International Research Network Rank: " << current->intResearchNetRank << endl;
-		cout << "Employment Outcome Score: " << current->employOutcomeScore << endl;
-		cout << "Employment Outcome Rank: " << current->employOutcomeRank << endl;
-		cout << "Score Scale: " << current->scoreScale << endl;
-		cout << string(55, '=') << endl;
+	while (contPrint = true && current != NULL) {
+		for (int i = 0; i < 50; i++) {
+			cout << endl << "University Name: " << current->instName << endl;
+			cout << "Rank: " << current->rank << endl;
+			cout << "Location Code: " << current->locationCode << endl;
+			cout << "Location: " << current->location << endl;
+			cout << "Academic Score " << current->academicScore << endl;
+			cout << "Academic Rank: " << current->academicRank << endl;
+			cout << "Employer Reputation Score: " << current->employerRepScore << endl;
+			cout << "Employer Reputation Rank: " << current->employerRepRank << endl;
+			cout << "Faculty/Student Ratio Score: " << current->facultyStuRIScore << endl;
+			cout << "Faculty/Student Ratio Rank: " << current->facultyStuRIRank << endl;
+			cout << "Citations Per Faculty Score: " << current->citePerFacultyScore << endl;
+			cout << "Citations Per Faculty Rank: " << current->citePerFacultyRank << endl;
+			cout << "International Faculty Ratio Score: " << current->intFacultyRIScore << endl;
+			cout << "International Faculty Ratio Rank: " << current->intFacultyRIRank << endl;
+			cout << "International Student Ratio Score: " << current->intStuRIScore << endl;
+			cout << "International Student Ratio Rank: " << current->intStuRIRank << endl;
+			cout << "International Research Network Score: " << current->intResearchNetScore << endl;
+			cout << "International Research Network Rank: " << current->intResearchNetRank << endl;
+			cout << "Employment Outcome Score: " << current->employOutcomeScore << endl;
+			cout << "Employment Outcome Rank: " << current->employOutcomeRank << endl;
+			cout << "Score Scale: " << current->scoreScale << endl;
+			//cout << string(55, '=') << endl;
+			// 
+			//move to next address
+			current = current->nextAdd;
+		}
+		cout << "Do you want to continue printing? (Any key to continue, n to exit):" << endl;
+		cin >> cont;
+		//while(cont != "n" || cont != "y") {
+		//	cout << "Please enter a valid response!" << endl;
+		//	cout << "Do you want to continue printing? (y/n):" << endl;
+		//	cin.clear();
+		//	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		//	cin >> cont;
+		//}
 
-
-
-		//move to next address
-		current = current->nextAdd;
+		if (cont == "n") {
+			break;
+		}	
 	}
 	cout << "List ended here." << endl;
 }
